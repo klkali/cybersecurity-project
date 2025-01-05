c906845ba1aad102a7588cb865f0a1fdc9c1b22a
kali <kali@gmail.com>
2025-01-05 18:34:02 +0530

    Add Whitenoise for static file handling

--- cybersecurity_project/settings.py
+++ cybersecurity_project/settings.py
@@ -133,3 +133,4 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
